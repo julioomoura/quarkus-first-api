c430e16e27fa0d73fbbb52c5ac884d25fc0cc48b
Julio <juliomourajom@gmail.com>
2022-01-20 23:06:28 -0300

    Using properties

--- src/main/java/br/com/juliomoura/api/HelloWorldController.java
+++ src/main/java/br/com/juliomoura/api/HelloWorldController.java
@@ -2,34 +2,21 @@ package br.com.juliomoura.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import org.jboss.logging.Logger;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import br.com.juliomoura.dtos.PersonDTO;
+import io.quarkus.logging.Log;
 
 @Path("/v1/hello")
 public class HelloWorldController {
 
-    private static final Logger LOG = Logger.getLogger(HelloWorldController.class);
+    @ConfigProperty(name = "greeting.message") 
+    String message;
 
     @GET
-    @Path("/{lastName}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response hello(
-        @QueryParam(value = "name") String name, 
-        @PathParam(value = "lastName") String lastName) 
-    {
-        LOG.info("First name=" + name + " Last name=" + lastName);
-
-        PersonDTO person =new PersonDTO(name, lastName);
-
-        return Response.ok(person).build();
+    public String hello(@QueryParam(value = "name") String name) {
+        Log.info("Saying hello to: " + name);
+        return message + " " + name;
     }
 }
-
-
--- src/main/java/br/com/juliomoura/api/PersonController.java
+++ src/main/java/br/com/juliomoura/api/PersonController.java
@@ -1,8 +1,6 @@
 package br.com.juliomoura.api;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
