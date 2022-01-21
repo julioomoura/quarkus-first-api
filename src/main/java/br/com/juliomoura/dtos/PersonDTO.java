34fdf94f1ba739cf81951fb855a6de82ce9c8015
Julio <juliomourajom@gmail.com>
2022-01-20 22:35:07 -0300

    Using lombok

--- src/main/java/br/com/juliomoura/dtos/PersonDTO.java
+++ src/main/java/br/com/juliomoura/dtos/PersonDTO.java
@@ -2,47 +2,17 @@ package br.com.juliomoura.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class PersonDTO {
     private String name;
     private String lastName;
-    
-    
-    public PersonDTO(String name, String lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
 
     @JsonIgnore
     public String getFullName() {
         return name + " " + lastName;
     }
-    
-    public String getName() {
-        return name;
-    }
-
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-
-    public String getLastName() {
-        return lastName;
-    }
-
-
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-
-    @Override
-    public String toString() {
-        return getFullName();
-    }
-
 }
